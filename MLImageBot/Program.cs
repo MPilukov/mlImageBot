4b57354f3a87cc8901fd5969b13b01df3a5cfa12
filiniti <filiniti@gmail.com>
2021-03-31 12:25:41 +0500

    image bot

---
+++ MLImageBot/Program.cs
@@ -0,0 +1,81 @@
+﻿using BaseBotLib.Interfaces.Logger;
+using BaseBotLib.Interfaces.Storage;
+using BaseBotLib.Services.Bot;
+using BaseBotLib.Services.Logger;
+using BaseBotLib.Services.Storage;
+using Microsoft.Extensions.Configuration;
+using MLImageLib;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MLImageBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var logger = GetLogger();
+
+            try
+            {
+                var configs = GetConfiguration();
+
+                var botId = configs["AppSettings:bot.id"];
+                var botToken = configs["AppSettings:bot.token"];
+                var storage = GetStorage(configs, logger);
+
+                var bot = new Bot(botId, botToken, storage, logger);
+
+                var inceptionPath = configs["AppSettings:ml.inceptionPath"];
+                var setsPath = configs["AppSettings:ml.setsPath"];
+                var modelPath = configs["AppSettings:ml.modelPath"];
+
+                var model = new Model(inceptionPath, setsPath, modelPath);
+                model.FitModel();
+
+                var ownerBot = new OwnerBot(bot, logger, model);
+
+                while (true)
+                {
+                    try
+                    {
+                        await ownerBot.CheckRequests();
+                    }
+                    catch (Exception exc)
+                    {
+                        logger.Error($"Ошибка при обработке сообщения : {exc}");
+                    }
+                    await Task.Delay(100);
+                }
+
+                logger.Info("Работа программы завершена. Нажмите любую клавишу для выхода.");
+                Console.ReadKey();
+            }
+            catch (Exception exc)
+            {
+                logger.Error("Не удалось запустить бота. Завершаем работу.");
+                logger.Error($"Exception : {exc}");
+            }
+        }
+
+        private static ILogger GetLogger()
+        {
+            return new ConsoleLogger();
+        }
+
+        private static IStorage GetStorage(IConfiguration configs, ILogger logger)
+        {
+            return new FileStorage(configs["AppSettings:storage.fileName"], logger);
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
+    }
+}
